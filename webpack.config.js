5d063b67a2dfd2377ac195703897e0d22b71e10d
FADAHUNSI <oluwaseyisamuel018@gmail.com>
2022-07-06 16:27:39 +0100

    Added the dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+      },
     plugins: [
         new HtmlWebpackPlugin({
-          title: 'Output Management',
+          template: './src/index.html'
         }),
       ],
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+      },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 }
